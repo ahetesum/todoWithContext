819613aa7cd9373bb9c9ff8fdf891e5e9343f912
hb <hb@hbs-MacBook-Pro-3.local>
2022-04-13 21:03:01 +0530

    minor changes

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -6,40 +6,43 @@ import {
   faClipboardCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import "./TodoItem.css";
+import { useEffect, useState } from "react";
 
 export type TodoProps = {
   id: number;
   name: string;
-  status: number;
+  status: boolean;
 };
 
 const TodoItem = (props: TodoProps) => {
   const { id, name, status } = props;
 
-  const setStausUpdate = () => {
-    //status == 0 ? (status = 1) : (status = 0);
-  };
+  const [isStatus,setIsSatus]=useState(status);
+
 
   return (
+
     <>
       <div className="todo-bg">
         <div className="textContainer">
-          <h4 className="todo-label">{name}</h4>
+          <h4 className={(isStatus)? "todo-label stripeText":"todo-label"}>{name}</h4>
         </div>
         <div className="actionContainer">
           <div className="actionSeconary">
-            {status == 0 ? (
+            {isStatus == false ? (
               <FontAwesomeIcon
                 className="imageTrash"
                 size="2x"
                 icon={faClipboardList}
-                onClick={() => setStausUpdate()}
+                onClick={() => setIsSatus(!isStatus) }
               />
             ) : (
               <FontAwesomeIcon
                 className="imageTrash"
                 size="2x"
                 icon={faClipboardCheck}
+                onClick={() => setIsSatus(!isStatus)}
+
               />
             )}
           </div>
